dbb2a3628ce4d78e914286722c2ec37d44140f8e
Stephen O'Malley <stephenomalley81@gmail.com>
2020-03-11 20:55:30 +0000

    Change GCP config lookup

--- config/config.exs
+++ config/config.exs
@@ -5,8 +5,7 @@ config :paypal_exporter, PaypalExporter.Scheduler,
     # Every minute
     #{"10 18 * * *",   fn -> PaypalExporter.Server.retrive_daily_transactions()  end},
   ]
-config :goth,
-json: "/home/stephen/playgrounds/elixir/paypal_exporter/priv/gcp.json" |> File.read!
+config :goth, json: {:system, "GCP_CREDENTIALS"}
 
 
 
